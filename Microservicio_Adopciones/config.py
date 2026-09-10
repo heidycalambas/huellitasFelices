71d5b9f343b96b8bf562ab803f2c693c15ab548a
evelyncel <evelyncampo@unicomfacauca.edu.co>
2026-09-09 22:31:45 -0500

    confy comentado
    Added comments to clarify the purpose of the code.

--- Microservicio_Adopciones/config.py
+++ Microservicio_Adopciones/config.py
@@ -1,15 +1,17 @@
-import os
-from dotenv import load_dotenv
+import os  # Permite trabajar con variables del sistema
+from dotenv import load_dotenv  # Carga las variables del archivo .env
 
-load_dotenv()
+load_dotenv()  # Carga las configuraciones guardadas en .env
 
 
 class Config:
 
+    # Datos para conectarse a la base de datos
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_USER = os.getenv("DB_USER", "root")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "")
     DB_NAME = os.getenv("DB_NAME", "huellitas_felices")
 
+    # Configuración del servidor
     SERVER_HOST = os.getenv("SERVER_HOST", "0.0.0.0")
     SERVER_PORT = int(os.getenv("SERVER_PORT", 8001))
