c947d5234bdd2dd45242d8d96965da7c3941f97d
evelyncel <evelyncampo@unicomfacauca.edu.co>
2026-09-09 23:17:13 -0500

    servermascotas comentado

--- Microservicio_Mascotas/server.py
+++ Microservicio_Mascotas/server.py
@@ -1,10 +1,11 @@
-from spyne import Application
-from spyne.protocol.soap import Soap11
-from spyne.server.wsgi import WsgiApplication
+from spyne import Application  
+from spyne.protocol.soap import Soap11  
+from spyne.server.wsgi import WsgiApplication  
 
-from service import PetService
+from service import PetService  # Importa el servicio de mascotas
 
 
+# Crea la aplicación principal del microservicio
 application = Application(
     [PetService],
     tns="huellitas.mascotas",
@@ -13,16 +14,17 @@ application = Application(
 )
 
 
-wsgi_application = WsgiApplication(application)
+wsgi_application = WsgiApplication(application)  # Prepara el servicio para ejecutarlo
 
 
 if __name__ == "__main__":
 
-    from wsgiref.simple_server import make_server
+    from wsgiref.simple_server import make_server  # Permite levantar el servidor
 
     host = "0.0.0.0"
     port = 8000
 
+  
     print("--------------------------------------------")
     print("MICROSERVICIO DE MASCOTAS")
     print("--------------------------------------------")
@@ -30,10 +32,11 @@ if __name__ == "__main__":
     print(f"WSDL: http://localhost:{port}/?wsdl")
     print("--------------------------------------------")
 
+    # Crea el servidor usando el puerto indicado
     server = make_server(
         host,
         port,
         wsgi_application
     )
 
-    server.serve_forever()
+    server.serve_forever()  
